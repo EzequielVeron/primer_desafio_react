55529e4f963bcb2e14ce195c501728b9d8cee28b
Ezequiel Veron <ezequieelveroon@gmail.com>
2022-08-25 20:47:41 -0300

    react01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import productos from "./data/personas.json";
 
-function App() {
+const App =() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Chocolates</p>
+      {
+        productos.map(producto =>{
+          <div><p>Marca: {producto.marca}</p>
+            <p>Producto:{producto.producto}</p>
+          </div>
+        })
+      }
     </div>
   );
 }
